7d436166ea9b8be40745464edf138dcfb5aee44d
JL <106701193+Jl115@users.noreply.github.com>
2025-01-27 18:32:06 +0100

    Update init.lua

--- lua/todo_viewer/init.lua
+++ lua/todo_viewer/init.lua
@@ -3,22 +3,17 @@ local M = {}
 local ui = require("todo_viewer.ui")
 
 M.toggle_todo_panel = function()
-	ui.toggle_todo_panel()
+    ui.toggle_todo_panel()
 end
 
 M.setup = function()
-	vim.api.nvim_set_keymap(
-		"n",
-		"<leader>t",
-		":lua require('todo_viewer').toggle_todo_panel()<CR>",
-		{ noremap = true, silent = true }
-	)
-	vim.api.nvim_set_keymap(
-		"n",
-		"l",
-		":lua require('todo_viewer.ui').open_todo_at_line()<CR>",
-		{ noremap = true, silent = true }
-	)
+    -- Only keep the global toggle keymap
+    vim.api.nvim_set_keymap(
+        "n",
+        "<leader>t",
+        ":lua require('todo_viewer').toggle_todo_panel()<CR>",
+        { noremap = true, silent = true }
+    )
 end
 
 return M
